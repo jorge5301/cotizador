5f24c69c241319b073e15d502b35767394dfedcb
Jorge1223 <71563549+jorge5301@users.noreply.github.com>
2023-05-30 12:14:50 -0400

    1

--- example/views.py
+++ example/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from django.http import HttpResponse
 
+
 def index(request):
     now = datetime.now()
     html = f'''
--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for vercel_app project.
-
-It exposes the WSGI callable as a module-level variable named ``app``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
 
 import os
 
